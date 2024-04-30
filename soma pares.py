12f28c51cb74a12f3cf77ac3191f8e53d89def53
xslmxslm <168542426+xslmxslm@users.noreply.github.com>
2024-04-30 08:27:37 -0300

    Add files upload soma pares.py

---
+++ soma pares.py
@@ -0,0 +1,11 @@
+#soma_pares
+titulo = "Soma Números Pares"
+print(f"{titulo:^30}")
+
+num= int(input("Digite um número"))
+i = 0
+soma=0i <= num:
+    if i % 2 == 0:
+        soma = soma + 1
+    i += 1
+print(f'A soma dos números pares até {num:>010d} é {soma:010d}')
